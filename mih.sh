a1641649600ae427411a584070ae86195708abbf
郭俊余 <hagbgreen@yahoo.com>
2016-07-01 10:36:44 +0800

    Update mih.sh

--- mih.sh
+++ mih.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 _mcchat_input="$(zenity --entry --text '保持 Minecraft 处于输入界面并在此输入内容：' --title 'Minecraft 输入辅助工具' )"
-if [ "$_mcchat_input" = "" ]; then:
+if [ "$_mcchat_input" = "" ]; then
 exit 1
 fi
 sleep 0.35
